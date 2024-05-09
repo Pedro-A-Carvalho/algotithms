61d417a59965ee4676c34dd31c4def3b190fc9d5
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-09 12:35:17 -0300

    commit bonus

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums:
+        return False
+    nums = sorted(nums)
+    for index, num in enumerate(nums):
+        if index == len(nums) - 1 or isinstance(num, str) or num < 0:
+            return False
+        if num == nums[index + 1]:
+            return num
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    first = 0
+    last = len(word) - 1
+    while first < last:
+        if word[first] != word[last]:
+            return False
+        first += 1
+        last -= 1
+    return True
