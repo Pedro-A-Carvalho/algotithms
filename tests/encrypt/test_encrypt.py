e69a850a5033bf8dc797654a8fca8cbdf7d7f3c1
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-09 10:54:59 -0300

    commit 2 req

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello", 2) == "oll_eh"
+    assert encrypt_message("world", 3) == "row_dl"
+    assert encrypt_message("python", 4) == "no_htyp"
+    assert encrypt_message("hello", 6) == "olleh"
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "2")
+    with pytest.raises(TypeError):
+        encrypt_message(123, 2)
