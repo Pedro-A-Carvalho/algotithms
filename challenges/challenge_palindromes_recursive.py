0c46ccb2bf8bf0fbfef54287422624b6f3e7ba35
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-09 11:16:35 -0300

    commit 2.1 req

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    if len(word) - 1 == high_index:
+        return word == is_palindrome_recursive(word, high_index, low_index)
+    return word[::-1]
